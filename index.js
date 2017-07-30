d7ca1ff4150a5e4c3c93c32aaed145e2a78e4b9a
bennyboy23 <b.j.e.evans-12@alumni.lboro.ac.uk>
2017-07-30 20:00:47 +0100

    altered to work on heroku

--- index.js
+++ index.js
@@ -3,29 +3,29 @@ const app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
 
-router.use(function(req,res,next) {
-    console.log("/" +req.method);
+router.use(function (req, res, next) {
+    console.log("/" + req.method);
     next();
 });
 app.set('port', (process.env.PORT || 5000));
-router.get("/",function(req,res){
+router.get("/", function (req, res) {
     res.sendFile(path + "index.html");
 });
 
-router.get("/about",function(req,res){
+router.get("/about", function (req, res) {
     res.sendFile(path + "about.html");
 });
 
-router.get("/contact",function(req,res){
+router.get("/contact", function (req, res) {
     res.sendFile(path + "contact.html");
 });
 
-app.use("/",router);
+app.use("/", router);
 
-app.use("*",function(req,res){
+app.use("*", function (req, res) {
     res.sendFile(path + "404.html");
 });
 
-app.listen(app.get("port"),function(){
+app.listen(app.get("port"), function () {
     console.log('example app listening');
 });
